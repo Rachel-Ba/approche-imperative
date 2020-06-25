83331a66888b5aaca19eaed783c4a6a06268d3f8
Rachel BANDIERA <rachel.bandiera@gmail.com>
2020-06-25 17:03:13 +0200

    J'ai confondu approche imperative et objet en nommant jenaimarre

--- src/entites/Personne.java
+++ src/entites/Personne.java
@@ -12,7 +12,50 @@ public class Personne {
 			this.nom = Nom;
 			this.prenom = Prenom;
 			this.adresse = Adresse;
+	
 		}
+		
+		//Dans la classe Personne, ajoutez une méthode qui permet d’afficher le nom et le
+		//prénom avec le nom de famille en majuscules.
+		
+		public void affichage()
+		{
+			System.out.println(nom.toUpperCase() + " " + prenom.toUpperCase());
+		}
+		
+		public void modifiNom(String modifnom)
+		{
+			nom = modifnom;
+		}
+		
+		public void modifiPrenom(String modifPrenom)
+		{
+			prenom = modifPrenom;
+		}
+
+		public void modifiAdresse(AdressePostale modifAdresse)
+		{
+			adresse = modifAdresse;
+		}
+		
+		public String retourNom(String renom)
+		{
+			return renom;
+		}
+		
+		public String retourPrenom(String rePrenom)
+		{
+			return rePrenom;
+		}
+		
+		public AdressePostale retourAdresse(AdressePostale reAdresse)
+		{
+			return reAdresse;
+		}
+
+		
+
+		
 }
 
 
--- src/essais/TestPersonne.java
+++ src/essais/TestPersonne.java
@@ -7,28 +7,28 @@ public class TestPersonne {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		AdressePostale ap1 = new AdressePostale(5,"place Agatha Christie",26000,"Valence");
-/*		ap1.numeroRue = 5;
-		ap1.libelleRue = "place Agatha Christie";
-		ap1.codePostal = 26000;
-		ap1.ville = "Valence";*/
 		
 		AdressePostale ap2 = new AdressePostale(457, "rue Auguste Broussonnet",34090,"Montpellier");
-/*		ap2.numeroRue = 457;
-		ap2.libelleRue = "rue Auguste Broussonnet";
-		ap2.codePostal = 34090;
-		ap2.ville = "Montpellier";*/
 		
 		///////////////////////////////////////////////////////////////////////////////////////
 		
 		Personne p1 = new Personne("Subedar","Naib",ap1);
-/*		p1.nom = "Subedar";
-		p1.prenom = "Naib";
-		p1.adresse = ap1;*/
 		
 		Personne p2 = new Personne("Clark","Eli",ap2);
-/*		p2.nom = "Clark";
-		p2.prenom = "Eli";
-		p2.adresse = ap2;*/
+
+		/////////////////////////////////////////////////////////////////////
+		
+		p1.affichage();
+		p2.affichage();
+		
+		p1.modifiNom("canard");
+		p1.modifiPrenom("coincoin");
+//		p1.modifiAdresse("tu fais le canard");
+		System.out.println(p1.nom);
+		
+		String nomTest = p1.retourNom("patate");
+		System.out.print(nomTest);
+		
 	}
 
 }
