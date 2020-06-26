59c6848cf40dee211295108ae5cb318179c2b9d0
Rachel BANDIERA <rachel.bandiera@gmail.com>
2020-06-26 12:11:47 +0200

    Genaimarredefairedesconnerie

---
+++ src/fr/diginamic/banque/Compte.java
@@ -0,0 +1,40 @@
+package fr.diginamic.banque;
+
+public class Compte {
+	
+	private int NumeroCompte;
+	
+	private int SoldeCompte;
+	
+	/////////////////////////////////
+
+	public int getNumeroCompte() {
+		return NumeroCompte;
+	}
+
+	public void setNumeroCompte(int numeroCompte) {
+		NumeroCompte = numeroCompte;
+	}
+
+	public int getSoldeCompte() {
+		return SoldeCompte;
+	}
+
+	public void setSoldeCompte(int soldeCompte) {
+		SoldeCompte = soldeCompte;
+	}
+
+	
+	//////////////////////////////////
+	
+	public Compte(int numeroCompte, int soldeCompte) {
+		super();
+		NumeroCompte = numeroCompte;
+		SoldeCompte = soldeCompte;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/fr/diginamic/banque/entites/TestBanque.java
@@ -0,0 +1,19 @@
+package fr.diginamic.banque.entites;
+
+import fr.diginamic.banque.*;
+
+public class TestBanque {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Compte cpt = new Compte(45689, 600);
+		
+		System.out.print(cpt);
+		
+		
+;
+
+	}
+
+}
