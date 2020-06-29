297d411c8d098ee02030a994c0268b2b13e2196c
Rachel BANDIERA <rachel.bandiera@gmail.com>
2020-06-29 19:23:24 +0200

    IT'S ALIIIIIIIIIIIIIIIVE

--- src/essais/TestCercle.java
+++ src/essais/TestCercle.java
@@ -1,16 +1,23 @@
 package essais;
 import entites.Cercle;
+import fr.diginamic.utils.CercleFactory;
 
 public class TestCercle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		double x = 4.3;
 		
-		Cercle c1 = new Cercle(4.3);
+		Cercle c1 = new Cercle(x);
 		Cercle c2 = new Cercle(6.6);
 		
 		System.out.println(c1);
 		System.out.println(c2);
+		
+		Cercle resultat = CercleFactory.niqueToi(5.0);
+		
+		System.out.println(resultat);
+		
 
 	}
 
---
+++ src/fr/diginamic/utils/CercleFactory.java
@@ -0,0 +1,13 @@
+package fr.diginamic.utils;
+import entites.Cercle;
+
+public class CercleFactory {
+	
+	  public static Cercle niqueToi(double x) {
+		  	Cercle c1 = new Cercle(x);
+		  	return c1;
+		  
+		  }
+
+
+}
